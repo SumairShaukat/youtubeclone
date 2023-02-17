6df45a6026aa61ef7d3cb0181ad1b31f5872630e
SumairShaukat <sumairch224@gmail.com>
2023-02-17 17:21:41 +0500

    routing setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <div className="App"> asasasAPP</div>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  Navbar,
+  Feed,
+  SearchFeed,
+  ChannelDetail,
+  VideoDetail,
+} from "./components/indeex";
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" exact element={<VideoDetail />} />
+        <Route path="/channel/:id" exact element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+        <Route path="/" exact element={<Feed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
